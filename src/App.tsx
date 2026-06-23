93ec9cb10395e0188edb6479e737497d7adc34b7
Dent Oral <dent.oral.bd@gmail.com>
2026-06-23 04:03:39 +0000

    Fix App.tsx - remove unused react-query import

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppRoutes } from './routes'
-
-const queryClient = new QueryClient()
+import './index.css'
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
